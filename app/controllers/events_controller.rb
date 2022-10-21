38b71f588cab339b0f62b47beafa5195eae0288d
Justin Lamb <justinlamb@me.com>
2022-10-21 10:31:43 -0700

    event creation enabled

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -20,6 +20,6 @@ class EventsController < ApplicationController
   private
 
   def event_params
-    params.require(:event).permit(:title, :description, :date)
+    params.require(:event).permit(:title, :description, :event_date)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_20_184215) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_21_172053) do
   create_table "events", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -18,6 +18,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_20_184215) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "creator_id"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
